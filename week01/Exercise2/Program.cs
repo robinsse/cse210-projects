90c6277eaf19b10862e89eadb890fcb895eb2175
robinsse <srobins@byupathway.edu>
2025-05-10 13:22:15 -0600

    Exercise 2 Letter Grades plus Stretch Challenges

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentageText = Console.ReadLine();
+        int gradePercentage = int.Parse(gradePercentageText);
+        int lastDigit = gradePercentage % 10;
+
+        string letter = "";
+        string refinedLetter = "";
+
+        //Find letter grade
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80 && gradePercentage < 90)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70 && gradePercentage < 80)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60 && gradePercentage < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        //Find if + or - exists
+        if (lastDigit >= 7 && letter != "A" && letter != "F")
+        {
+            refinedLetter = "+";
+        }
+        else if (lastDigit < 3 && letter != "F")
+        {
+            refinedLetter = "-";
+        }
+        else
+        {
+            refinedLetter = "";
+        }
+
+        //Grade Statement
+        Console.WriteLine($"You got a {letter}{refinedLetter}. ");
+
+        //Cheerleader Statement
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations. You passed! ");
+        }
+        else
+        {
+            Console.WriteLine("You should try again to pass the class. You can do it!");
+        }
     }
 }
