93c4a15934b6ebb9554c5013696cc01605f1f585
robinsse <srobins@byupathway.edu>
2025-05-10 15:37:08 -0600

    Exercise 3 Core Challenges

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        //Generate random number for magic number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        //Initialize guess variable
+        int guess = -1;
+
+        //while Loop
+        while (guess != magicNumber)
+        {
+            //Ask user for a guess
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            //If statement Higher or Lower
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed the magic number!");
+            }
+
+        }
     }
 }
