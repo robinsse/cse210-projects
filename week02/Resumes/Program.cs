2db01ac8b9db7bf522b8fcc5ad7bef7e539b3672
robinsse <srobins@byupathway.edu>
2025-05-17 07:45:53 -0600

    Resume Learning Activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "BYU Independent Study";
+        job1._jobTitle = "Graphic Designer";
+        job1._startYear = 2002;
+        job1._endYear = 2003;
+
+        Job job2 = new Job();
+        job2._company = "BYU-Idaho";
+        job2._jobTitle = "Online Curriculum Designer";
+        job2._startYear = 2019;
+        job2._endYear = 2025;
+
+        // job1.Display();
+        // job2.Display();
+
+        Resume name1 = new Resume();
+        name1._name = "Selena Robins";
+
+        name1._jobs.Add(job1);
+        name1._jobs.Add(job2);
+
+        name1.Display();
     }
 }
