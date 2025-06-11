5042e81187694199f228d13c7a9c4638e18d69a5
robinsse <srobins@byupathway.edu>
2025-06-11 15:58:40 -0600

    Shapes Learning Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("blue", 4);
+        Rectangle rectangle = new Rectangle("pink", 3, 4);
+        Circle circle = new Circle("teal", 10);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The shape is {color} and its area is {area}.");
+        }
     }
 }
